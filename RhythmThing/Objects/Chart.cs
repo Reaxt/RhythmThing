3de78d2b8791eb0f83b74df65d98a26c2d31733f
Reaxt <koehlertrevor@gmail.com>
2020-05-16 17:13:03 -0400

    Added freeze event.
    Its kinda shit with how its written? This may be a horrible horrible idea but yknow what it performs fine so far. Also added the relativeAllCOllumnMove thing which was neetded.

--- RhythmThing/Objects/Chart.cs
+++ RhythmThing/Objects/Chart.cs
@@ -151,7 +151,7 @@ namespace RhythmThing.Objects
             song = game.audioManager.addTrack(Path.Combine(chartPath, chartInfo.songPath));
             //debug obj
             game.addGameObject(new ChartDebug(this));
-            float startBeat = 320;
+            float startBeat = 354;
             //song.sampleSource.SetPosition(TimeSpan.FromMilliseconds(startBeat / ((float)(chartInfo.bpm) / 60000)));
 
         }
